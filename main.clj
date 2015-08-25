97f49c213c8479ae629db772ca77fe935cb0c7f8
Mark Woodbridge <mrw34@users.noreply.github.com>
2015-08-25 23:05:45 +0100

    Use update-in

--- main.clj
+++ main.clj
@@ -1,16 +1,14 @@
 (require '[clojure.string :as str])
 
-;; Tip - refactor assoc into update-in
-
-(def db (reduce (fn [database pair] (assoc database (first pair) (cons (second pair) (get database (first pair)))))
-  {}
-  (partition 2 1
-           (filter (complement str/blank?)
-                   (str/split (slurp "sample_tweets.txt") #"\s")))))
+(def db 
+  (reduce
+    (fn [database pair] (update-in database [(first pair)] #(cons (second pair) %)))
+    {}
+    (partition 2 1
+      (str/split (slurp "sample_tweets.txt") #"\s+"))))
 
 (defn next-word [current-word]
-  (if (.endsWith current-word ".") [current-word] (cons current-word (next-word (rand-nth (db current-word)))))
-  )
+  (cons current-word (if (.endsWith current-word ".") [] (next-word (rand-nth (db current-word))))))
 
 (defn tweetgen [first-word] (str/join " " (next-word first-word)))
 
