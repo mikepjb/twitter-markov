f94c051af2010e871b7cd8cbff5e79b1e5acd994
Michael Bruce <michael.bruce@singletracksystems.com>
2015-08-25 20:33:10 +0100

    printing sequences of words

--- main.clj
+++ main.clj
@@ -1,10 +1,17 @@
 ;; (+ 40 2)
 (require '[clojure.string :as str])
 
+;; Tip - refactor assoc into update-in
+
 ;;Read lines
-(reduce (fn [database pair] (assoc database (first pair) (cons (second pair) (get database (first pair)))))
+(def db (reduce (fn [database pair] (assoc database (first pair) (cons (second pair) (get database (first pair)))))
   {}
   (partition 2 1
            (filter #(not (str/blank? %))
-                   (str/split ( slurp "sample_tweets.txt") #"\W"))))
+                   (str/split ( slurp "sample_tweets.txt") #"\W")))))
+
+;; Prints var
+(def word (rand-nth (keys db)))
+
+(rand-nth (db word))
 
