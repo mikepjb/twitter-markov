df3c05e47f44e0fdc0b475a36edd7a48b19129d6
Michael Bruce <michael.bruce@singletracksystems.com>
2015-08-25 21:14:54 +0100

    final commit

--- main.clj
+++ main.clj
@@ -1,24 +1,17 @@
-;; (+ 40 2)
 (require '[clojure.string :as str])
 
 ;; Tip - refactor assoc into update-in
 
-;;Read lines
 (def db (reduce (fn [database pair] (assoc database (first pair) (cons (second pair) (get database (first pair)))))
   {}
   (partition 2 1
            (filter #(not (str/blank? %))
-                   (str/split ( slurp "sample_tweets.txt") #"\s")))))
+                   (str/split (slurp "sample_tweets.txt") #"\s")))))
 
 (defn next-word [current-word]
   (if (.endsWith current-word ".") [current-word] (cons current-word (next-word (rand-nth (db current-word)))))
   )
 
-;; (next-word
-;; (rand-nth (keys db)) 10)
-
 (defn tweetgen [first-word] (str/join " " (next-word first-word)))
 
-
-
-
+(tweetgen "Split")
